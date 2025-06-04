6383a59752359a066856ebb27b5484545aa19b64
faheelawaseem <faheela@techieonix.io>
2025-06-04 18:33:10 +0500

    TEX-340 created view own blogs api

--- src/app/api/users/[userid]/route.ts
+++ src/app/api/users/[userid]/route.ts
@@ -1,5 +1,24 @@
-// import { NextResponse } from "next/server";
-// import { connectDb } from "@/src/app/configs/database";
+import { NextRequest, NextResponse } from "next/server";
+import { connectDb } from "@/src/app/configs/database";
+import { Blog } from "@/src/models/blog";
+
+export async function GET(request: NextRequest, {params}: {params: {userid: string}}) {
+  await connectDb();
+  const userid = params.userid;
+
+  if (!userid) {
+    return NextResponse.json({ error: "User ID is required" }, { status: 400 });
+  }
+
+  const userBlogs = await Blog.find({ authorId: userid });
+  if (userBlogs.length === 0) {
+    return NextResponse.json(
+      { message: "No blogs found for this user" },
+      { status: 404 }
+    );
+  }
+  return NextResponse.json(userBlogs, { status: 200 });
+}
 
 // export async function DELETE(
 //     request: Request,
