2597b96b8075c9dcc74af3908d2102cd58d4a96c
sanasraza <sanasraza@techieonix.io>
2025-06-05 20:27:46 +0500

    TEX-340: Remove comments for now

--- models/blog.ts
+++ models/blog.ts
@@ -44,12 +44,6 @@ const BlogSchema = new Schema({
         type: Number,
         default: 0
     },
-    // comments: [{
-    //     userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
-    //     comment: { type: String, required: true },
-    //     createdAt: { type: Date, default: Date.now },
-    //     updatedAt: { type: Date }
-    // }],
     status: {
         type: String,
         enum: ["Draft", "Published", "Archived"],
